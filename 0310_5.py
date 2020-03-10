b73634a6e3aa55ee8f77cda2b116b5d9d75377d4
surimLee <45868707+surimLee@users.noreply.github.com>
2020-03-10 22:49:32 +0900

    Add files via upload

---
+++ 0310_5.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk()
+b1 = Button(window, text="첫번째 버튼")
+b2 = Button(window, text="두번째 버튼")
+b1.pack(side=LEFT,padx=10)
+b2.pack(side=LEFT,padx=10)
+b1["text"] = "One"
+b2["text"] = "Two"
+
+window.mainloop()
