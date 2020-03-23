05916b9a1876d1aacb068ccf88188f87c25735a4
surimLee <45868707+surimLee@users.noreply.github.com>
2020-03-23 23:57:52 +0900

    IoT 설계 1주차 과제(4)

---
+++ scopes3.py
@@ -0,0 +1,18 @@
+# scopes3.py
+# Local, Enclosing and Global
+
+def enclosing_func():
+    m = 13
+
+    def local():
+        # m doesn't belong to the scope defined by the local function
+        # so Python will keep looking into the next enclosing scope.
+        # m is finally found in the global scope
+        print(m, 'printing from the local scope')
+    # calling the function local
+    local()
+
+m=5
+print(m, 'printing from the global scope')
+
+enclosing_func()
