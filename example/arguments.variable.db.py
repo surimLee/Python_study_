bf8a2d0ece4ba84163e1ea2ecb1f5371a2ebd139
surimLee <45868707+surimLee@users.noreply.github.com>
2020-03-30 23:58:25 +0900

    IoT설계 2주차 과제

---
+++ example/arguments.variable.db.py
@@ -0,0 +1,14 @@
+# arguments.variable.db.py
+
+def connect(**options):
+    conn_params = {
+        'host': options.get('host', '127.0.0.1'),
+        'port': options.get('port', 5432),
+        'user': options.get('user', ''),
+        'pwd': options.get('pwd', ''),
+    }
+    print(conn_params)
+
+connect()
+connect(host='127.0.0.42', post=5433)
+connect(port=5431, user='fab', pwd='gandalf')
