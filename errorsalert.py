bbdfcfc177b1955a6693d53eca8c37a0b8d282ec
surimLee <45868707+surimLee@users.noreply.github.com>
2020-04-01 23:51:49 +0900

    IoT 설계 2주차 과제

---
+++ errorsalert.py
@@ -0,0 +1,15 @@
+# errorsalert.py
+
+alert_system = 'console' # other value can be 'email'
+error_severity = 'critical' #other values : 'medium' or 'low'
+error_message = 'OMG! Something terrible happened!'
+
+if alert_system == 'console':
+    print(error_message)    #1
+elif alert_system == 'email':
+    if error_severity == 'critical':
+        send_email('admin@example.com', error_message)  #2
+    elif error_severity == 'medium':
+        send_email('support.1@example.com', error_message)  #3
+    else:
+        send_email('support.2@example.com', error_message)  #4
